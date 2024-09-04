380e168cce1229fcd8c24172ae37860ac00c2778
wenzexu <ricol.xwz@outlook.com>
2024-09-04 22:46:50 +1000

    create 1 file and update 1 file

---
+++ overrides/javascripts/umami.js
@@ -0,0 +1,13 @@
+function umami() {
+  var script = document.createElement("script");
+  script.defer = true;
+  script.src = "https://umami.ricolxwz.io/script.js";
+  script.setAttribute(
+    "data-website-id",
+    "83924929-2051-4bac-be81-bee6fbccf6c7"
+  );
+
+  document.head.appendChild(script);
+}
+
+umami();
