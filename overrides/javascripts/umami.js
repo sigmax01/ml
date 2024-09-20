5ce5ccad882258ce3fa1606e42d5d189c7e5667e
wenzexu <sigmax0124@hotmail.com>
2024-09-21 08:20:03 +1000

    create 1 file and update 1 file

--- overrides/javascripts/umami.js
+++ overrides/javascripts/umami.js
@@ -2,7 +2,7 @@ function umami() {
   if (window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1") {
     var script = document.createElement("script");
     script.defer = true;
-    script.src = "https://umami.ricolxwz.io/script.js";
+    script.src = "./analysis.js";
     script.setAttribute(
       "data-website-id",
       "c718d24c-0695-4b1a-bc57-30f0a0ea4cfb"
